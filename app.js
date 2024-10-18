4d4611bba409b566fe816734acd1908f55ef52b3
jstrohsnitter <jstrohsnitter@gmail.com>
2024-10-18 16:43:25 -0400

    Exercise 4

--- app.js
+++ app.js
@@ -35,3 +35,18 @@ Complete Exercise 3 in the space below:
 const newFood = foods2.unshift('taco');
 
 console.log('Exercise 3 result:', foods2);
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+const favFood = foods2[1];
+
+console.log('Exercise 4 result:', favFood);
