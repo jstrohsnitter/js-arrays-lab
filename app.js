d1ed95cee43b430bb246758f4ba387ce92fed63b
jstrohsnitter <jstrohsnitter@gmail.com>
2024-10-20 15:19:31 -0400

    Exercise 12

--- app.js
+++ app.js
@@ -163,3 +163,59 @@ return odds
 }
 const oddNumbers = findOdds(nums)
 console.log('Exercise 11 result:', oddNumbers);
+
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+*/
+
+function divThree(nums) {
+const fizz = []
+for (i = 0; i < 12; i++) {
+if (nums[i] % 3 === 0)
+    fizz.push(nums[i])
+}
+return fizz
+}
+
+function divFive(nums) {
+    const buzz = []
+    for (i = 0; i < 12; i++) {
+    if (nums[i] % 5 === 0)
+        buzz.push(nums[i])
+    }
+    return buzz
+    }
+
+
+    function divThreeFive(nums) {
+        const fizzbuzz = []
+        for (i = 0; i < 12; i++) {
+        if (nums[i] % 3 === 0 && nums[i] % 5 === 0)
+            fizzbuzz.push(nums[i])
+        }
+        return fizzbuzz
+        }
+
+const firstFizz = divThree(nums)
+const firstBuzz = divFive(nums)
+const firstFizzBuzz = divThreeFive(nums)
+
+console.log('Exercise 12 Results:');
+console.log('  fizz:', firstFizz);
+console.log('  buzz:', firstBuzz);
+console.log('  fizzbuzz:', firstFizzBuzz);
